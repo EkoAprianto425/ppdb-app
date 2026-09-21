9437739892346ab812cf95f692faef8d4f585118
ekozawa72 <ekozawa72@gmail.com>
2026-09-22 06:48:23 +0700

    perbaikan query list user pada menu manajemen kelulusan hanya untuk siswa yang sudah memiliki jadwal ujian

--- app/Http/Controllers/Admin/StudentManagementController.php
+++ app/Http/Controllers/Admin/StudentManagementController.php
@@ -367,7 +367,8 @@ class StudentManagementController extends Controller
     {
         $user = auth()->user();
         $query = Registration::with('user', 'academicYear', 'registrationWave', 'user.educationalLevel')
-            ->where('payment_status', 'success');
+            ->where('payment_status', 'success')
+            ->whereNotNull('exam_schedule_id');
 
         // Filter Status Kelulusan (proses, lulus, tidak_lulus)
         if ($request->filled('status')) {
